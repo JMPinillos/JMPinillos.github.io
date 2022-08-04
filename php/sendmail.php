21e9389e90014c212dcc2208b401c2dc68c26838
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2022-08-04 08:08:32 +0200

    Update sendmail.php

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,16 +1,16 @@
 <?php
 // Email Submit
 
-  $name = @trim(stripslashes($_POST['name'])); 
-  $email = @trim(stripslashes($_POST['email']));
-  $message = @trim(stripslashes($_POST['message'])); 
+  $name = @trim(stripslashes($_POST['Nombre'])); 
+  $email = @trim(stripslashes($_POST['Email']));
+  $message = @trim(stripslashes($_POST['Mensaje'])); 
 
   $email_from = $email;
   $email_to = 'pinillosrubio@gmail.com'; //replace with your email
 
-  $message = 'Nombre: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Message: ' . $message;
+  $message = 'Nombre: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Mensaje: ' . $message;
 
   $success = @mail($email_to, "[KasCo Landing] ".$name, $message);
 
-  $noreply = @mail($email_from, "[KasCo Landing] Thank you to email us.", "We will be back to you soon!");
+  $noreply = @mail($email_from, "[KasCo Landing] Gracias por contactarnos.", "¡Le responderemos pronto!");
 ?>
